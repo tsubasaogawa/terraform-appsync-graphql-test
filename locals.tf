c112fbd649ef80aeae2648a1fab21ff0bf64af6d
Tsubasa Ogawa <hurikake@gmail.com>
2020-07-25 21:52:39 +0900

    change query name

--- locals.tf
+++ locals.tf
@@ -1,22 +1,25 @@
 locals {
   project_name         = "graphql-test"
   project_name_for_uri = replace(local.project_name, "-", "_")
+  datasource_name      = "datasource_${local.project_name_for_uri}"
+
   dynamodb = {
     table_name = local.project_name
   }
+
   appsync = {
     name            = "appsync-${local.project_name}"
-    datasource_name = "datasource_${local.project_name_for_uri}"
+    datasource_name = local.datasource_name
     resolvers = {
-      single_user = {
-        field       = "singleUser"
+      user = {
+        field       = "user"
         type        = "Query"
-        data_source = "datasource_${local.project_name_for_uri}"
+        data_source = local.datasource_name
       }
       create_user = {
         field       = "createUser"
         type        = "Mutation"
-        data_source = "datasource_${local.project_name_for_uri}"
+        data_source = local.datasource_name
       }
     }
   }
